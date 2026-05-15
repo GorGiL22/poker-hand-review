e9a4909c46c7a16562dc22262bf273b26c1b12bb
GorGiL22 <103217313+GorGiL22@users.noreply.github.com>
2026-05-15 15:26:27 +0200

    wip: sauvegarde auto (Write) 2026-05-15T13-26-27Z

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2886,7 +2886,7 @@ export default function Home() {
         </section>
       </div>
 
-      {hands.length > 0 && !viewPublicHome && (
+      {hands.length > 0 && !viewPublicHome && !viewSpotReview && (
         <div className="pointer-events-none fixed bottom-4 left-3 z-[70] flex flex-col gap-2 sm:bottom-5 sm:left-5">
           <button
             type="button"
