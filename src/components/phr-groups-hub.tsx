5ef8d9ba204a612c164e7bdbb6eca07db34a9b3e
GorGiL22 <103217313+GorGiL22@users.noreply.github.com>
2026-05-15 18:54:34 +0200

    wip: sauvegarde auto (Write) 2026-05-15T16-54-34Z

--- src/components/phr-groups-hub.tsx
+++ src/components/phr-groups-hub.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 
 import {
   createReviewGroup,
+  formatFirestoreGroupError,
   generateInviteCode,
   joinReviewGroupByInviteCode,
   normalizeInviteCode,
