ce84bf8545b7d5b8b9370e11368b502ca771f257
GorGiL22 <103217313+GorGiL22@users.noreply.github.com>
2026-05-15 22:11:38 +0200

    wip: sauvegarde auto (Write) 2026-05-15T20-11-38Z

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2427,7 +2427,7 @@ export default function Home() {
           selectedHandId={selectedHand.id}
         />
       )}
-      <motion.div className="mx-auto flex min-h-0 w-full max-w-[1520px] flex-1 flex-col px-3 pb-3 pt-2 sm:px-5 sm:pb-4 sm:pt-3">
+      <div className="mx-auto flex min-h-0 w-full max-w-[1520px] flex-1 flex-col px-3 pb-3 pt-2 sm:px-5 sm:pb-4 sm:pt-3">
         <section className={PHR_APP_SHELL}>
           <input
             ref={fileInputRef}
